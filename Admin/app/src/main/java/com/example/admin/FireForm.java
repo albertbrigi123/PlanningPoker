c6d76f8401ce1c04b4fe48a975452616abb2621e
albertbrigi123 <albertbrigi95@gmail.com>
2019-11-19 23:34:12 +0200

    c
    Impemented Main Activity.
    Created and implemented Form Fragment(Admin-new group).
    Created Question Fragment.
    Conncted to database.
    Insert name and groupname in the database

--- Admin/app/src/main/java/com/example/admin/FireForm.java
+++ Admin/app/src/main/java/com/example/admin/FireForm.java
@@ -2,5 +2,32 @@ package com.example.admin;
 
 public class FireForm
 {
+    private String name;
+    private String groupName;
 
+    public FireForm()
+    {
+
+    }
+
+    public FireForm(String name, String groupName) {
+        this.name = name;
+        this.groupName = groupName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
 }
--- Admin/app/src/main/java/com/example/admin/Form.java
+++ Admin/app/src/main/java/com/example/admin/Form.java
@@ -1,5 +1,7 @@
 package com.example.admin;
-
+import com.example.admin.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,28 +13,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Form extends Fragment {
    private EditText name;
    private EditText gname;
    private Button createGroupBtn;
-    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+   private DatabaseReference dbform;
+   FirebaseDatabase firebaseDatabase;
+   DatabaseReference databaseReference;
+   public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_form,container,false);
-
         name=v.findViewById(R.id.name);
         gname=v.findViewById(R.id.groupName);
         createGroupBtn=v.findViewById(R.id.createGroupButton);
-
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference("Groups");
         createGroupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                addForm();
                 FragmentTransaction fr=getFragmentManager().beginTransaction();
                 fr.replace(R.id.fragment_container,new QuestionFragment());
                 fr.commit();
             }
         });
         return v;
+    }
+
+    public void addForm()
+    {
+        name=getView().findViewById(R.id.name);
+        String nameS=name.getText().toString();
+        gname=getView().findViewById(R.id.groupName);
+        String groupNameS=gname.getText().toString();
+        FireForm fm=new FireForm(nameS,groupNameS);
+        databaseReference.push().setValue(fm);
+        Toast.makeText(getActivity(),"Insert to database",Toast.LENGTH_SHORT).show();
+
     }
 
 }
